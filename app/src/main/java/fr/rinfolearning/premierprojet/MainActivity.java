f6a4a59648ea8db1c39951124947d26ff4744097
bl <bouchkhaf@gmail.com>
2024-04-09 12:25:00 +0200

    Test Button

--- app/src/main/java/fr/rinfolearning/premierprojet/MainActivity.java
+++ app/src/main/java/fr/rinfolearning/premierprojet/MainActivity.java
@@ -2,12 +2,28 @@ package fr.rinfolearning.premierprojet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) this.findViewById(R.id.button_affiche_ok);
+        final TextView affichage = (TextView) this.findViewById(R.id.tv_affichage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                affichage.setText(R.string.mess_after_clic_ok);
+            }
+
+
+        });
     }
 }
